d34876dbad06b57ed896cf54dabba9f46a9efd92
Akari Asai <akari.1311aquamarine4@gmail.com>
2018-01-03 23:59:39 +0900

    add progress bar

--- hungualian_experiment.py
+++ hungualian_experiment.py
@@ -1,5 +1,6 @@
 import sys
 import subprocess
+from tqdm import tqdm
 
 argvs = sys.argv
 max_idx = argvs[1]
@@ -8,7 +9,7 @@ tmp_output_filename = 'align_tmp_' + str(max_idx) + '.txt'
 result_filename = 'align_result_' + str(max_idx) + '.txt'
 result_f = open(result_filename, 'w')
 
-for i in range(10):
+for i in tqdm(range(10)):
     ja_filename = '/home/dl-exp/wiki_process/corpora_each/ja/ja_' + str(i) + '.txt'
     en_filename = '/home/dl-exp/wiki_process/corpora_each/en/en_' + str(i) + '.txt'
     dic_filename = '/home/dl-exp/hunalign_akari/hunalign/data/en-ja-endict.txt'
@@ -22,7 +23,9 @@ for i in range(10):
     f = open(tmp_output_filename)
     line = f.readline()
 
+
     while line:
+        print(line)
         result_f.write(line)
     f.close()
 
