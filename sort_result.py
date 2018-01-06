1878d914b5cfb827c81b2fcace35e67c0591aba9
Akari Asai <akari.1311aquamarine4@gmail.com>
2018-01-07 03:16:55 +0900

    fix an error

--- sort_result.py
+++ sort_result.py
@@ -27,6 +27,6 @@ result_f = open("sorted_alignment_result.csv", 'w')
 writer = csv.writer(result_f, quoting=csv.QUOTE_NONNUMERIC)
 
 for key, value in sorted(d.items(), key=itemgetter(1), reverse = True):
-    writer.writerow((pair_dic[key][0], pair_dic[key][1], score_dic[key])
+    writer.writerow((pair_dic[key][0], pair_dic[key][1], score_dic[key]))
 
 result_f.close()
