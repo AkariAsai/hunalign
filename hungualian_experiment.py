ac914a8e3c31ac7a10dc16dad20e8a93f30ef90a
Akari Asai <akari.1311aquamarine4@gmail.com>
2018-01-07 02:59:09 +0900

    fix the codes to write result using csv modules

--- hungualian_experiment.py
+++ hungualian_experiment.py
@@ -1,6 +1,7 @@
 import sys
 import subprocess
 from tqdm import tqdm
+import csv
 
 argvs = sys.argv
 start_idx = int(argvs[1])
@@ -8,6 +9,7 @@ start_idx = int(argvs[1])
 tmp_output_filename = 'align_tmp_' + str(start_idx) + '.txt'
 result_filename = 'align_result_' + str(start_idx) + '.txt'
 result_f = open(result_filename, 'w')
+writer = csv.writer(result_f, quoting=csv.QUOTE_NONNUMERIC)
 
 for i in tqdm(range(start_idx, start_idx+10000)):
     ja_filename = '/home/dl-exp/wiki_process/corpora_each/ja/ja_' + str(i) + '.txt'
@@ -26,7 +28,8 @@ for i in tqdm(range(start_idx, start_idx+10000)):
     while line:
         result = line.split('\t')
         if len(result[0]) > 0 and float(result[2]) > -0.3:
-            result_f.write(result[0] +','+ result[1] + ',' + result[2])
+            writer.writerow(i,result[0], result[1], float(result[2]))
+            # result_f.write(str(i) + '\t' + result[0] +'\t'+ result[1] + '\t' + result[2])
         line = f.readline()
     f.close()
 
