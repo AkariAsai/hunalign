f15c1c4da7ece7d4d7dfb6f927393afa91f8da45
Akari Asai <akari.1311aquamarine4@gmail.com>
2018-01-07 17:22:38 +0900

    add script to remove a header of en_file

--- remove_empty_lheader_lines.py
+++ remove_empty_lheader_lines.py
@@ -5,6 +5,7 @@ from tqdm import tqdm
 en_file_list = os.listdir('/home/dl-exp/wiki_process/corpora_each/en')
 
 for en_file in en_file_list:
-    sys_call = "tail -n +3 " +en_file + " > " + en_file.split(".")[0] + "_removed.txt"
+    full_en_file_name = '/home/dl-exp/wiki_process/corpora_each/en/' + en_file
+    sys_call = "tail -n +3 " + full_en_file_name + " > " + full_en_file_name.split(".")[0] + "_removed.txt"
     subprocess.call(sys_call, shell=True)
     exit()
