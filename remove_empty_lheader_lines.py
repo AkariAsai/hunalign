91459b034e37d339844ff959908f82e2afcc616e
Akari Asai <akari.1311aquamarine4@gmail.com>
2018-01-07 17:30:47 +0900

    fix reditrected file path

--- remove_empty_lheader_lines.py
+++ remove_empty_lheader_lines.py
@@ -8,8 +8,7 @@ for en_file in en_file_list:
     full_en_file_name = '/home/dl-exp/wiki_process/corpora_each/en/' + en_file
     print(full_en_file_name.split(".")[0])
     print(full_en_file_name.split(".")[0]+ "_removed.txt")
-    # sys_call = "tail -n +3 " + full_en_file_name + " > " + full_en_file_name.split(".")[0] + "_removed.txt"
-    sys_call = "tail -n +3 " + full_en_file_name + " > " + "removed.txt"
-
+    sys_call = "tail -n +3 " + full_en_file_name + " > " + full_en_file_name.split(".")[0]+ "_removed.txt"
     subprocess.call(sys_call, shell=True)
+    print(full_en_file_name.split(".")[0]+ "_removed.txt")
     exit()
