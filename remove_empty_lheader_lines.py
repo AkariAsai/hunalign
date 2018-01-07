b111a29f9192bdd53d722336badb38fbf8ac2dac
Akari Asai <akari.1311aquamarine4@gmail.com>
2018-01-07 17:31:53 +0900

    finish debug for convertign en_wiki sorporas

--- remove_empty_lheader_lines.py
+++ remove_empty_lheader_lines.py
@@ -4,11 +4,7 @@ from tqdm import tqdm
 
 en_file_list = os.listdir('/home/dl-exp/wiki_process/corpora_each/en')
 
-for en_file in en_file_list:
+for en_file in tqdm(en_file_list):
     full_en_file_name = '/home/dl-exp/wiki_process/corpora_each/en/' + en_file
-    print(full_en_file_name.split(".")[0])
-    print(full_en_file_name.split(".")[0]+ "_removed.txt")
     sys_call = "tail -n +3 " + full_en_file_name + " > " + full_en_file_name.split(".")[0]+ "_removed.txt"
     subprocess.call(sys_call, shell=True)
-    print(full_en_file_name.split(".")[0]+ "_removed.txt")
-    exit()
