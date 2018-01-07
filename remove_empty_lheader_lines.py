ef4eef1ff787bac4e59b1a546fdd095c638d85a5
Akari Asai <akari.1311aquamarine4@gmail.com>
2018-01-07 17:29:29 +0900

    debugging a script

--- remove_empty_lheader_lines.py
+++ remove_empty_lheader_lines.py
@@ -6,6 +6,8 @@ en_file_list = os.listdir('/home/dl-exp/wiki_process/corpora_each/en')
 
 for en_file in en_file_list:
     full_en_file_name = '/home/dl-exp/wiki_process/corpora_each/en/' + en_file
+    print(full_en_file_name.split(".")[0])
+    print(full_en_file_name.split(".")[0]+ "_removed.txt")
     # sys_call = "tail -n +3 " + full_en_file_name + " > " + full_en_file_name.split(".")[0] + "_removed.txt"
     sys_call = "tail -n +3 " + full_en_file_name + " > " + "removed.txt"
 
