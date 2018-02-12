b11c8062d18cd062a0f22bef69a740a4c795cad0
Akari Asai <akari.1311aquamarine4@gmail.com>
2018-02-12 21:49:19 +0900

    fix hunalign execution script

--- hungualian_experiment.py
+++ hungualian_experiment.py
@@ -6,12 +6,12 @@ import csv
 argvs = sys.argv
 start_idx = int(argvs[1])
 
-tmp_output_filename = 'align_tmp_weight_changed_' + str(start_idx) + '.txt'
-result_filename = 'align_result_weight_changed_' + str(start_idx) + '.txt'
+tmp_output_filename = '20180212/align_' + str(start_idx) + '.txt'
+result_filename = '20180212/align_' + str(start_idx) + '.txt'
 result_f = open(result_filename, 'w')
 writer = csv.writer(result_f, quoting=csv.QUOTE_NONNUMERIC)
 
-for i in tqdm(range(start_idx, start_idx + 10000)):
+for i in tqdm(range(start_idx, start_idx + 100000)):
     ja_filename = '/home/dl-exp/wiki_process/corpora_each/ja/ja_' + \
         str(i) + '.txt'
     en_filename = '/home/dl-exp/wiki_process/corpora_each/en/en_' + \
@@ -33,7 +33,8 @@ for i in tqdm(range(start_idx, start_idx + 10000)):
         if len(result[0]) > 0 and float(result[2]) > -0.3:
             writer.writerow(
                 (i, result[0], result[1], float(result[2])))
-            # result_f.write(str(i) + '\t' + result[0] +'\t'+ result[1] + '\t' + result[2])
+            result_f.write(str(i) + '\t' +
+                           result[0] + '\t' + result[1] + '\t' + result[2])
         line = f.readline()
     f.close()
 
